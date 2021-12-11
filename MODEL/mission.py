541ccb9a7cbd02c7c6718829b119a2511650dfe1
user <user@MacBook-Pro-de-user.local>
2021-12-11 13:50:28 +0000

    feat: add misson  model

---
+++ MODEL/mission.py
@@ -0,0 +1,23 @@
+from config import db
+
+
+class Missions(db.Document):
+    id=db.ObjectId()
+    id_people = db.StringField()
+    name = db.StringField()
+    description = db.StringField()
+    model = db.ListField()
+    parcour = db.ListField()
+    surface = db.StringField()
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "id_people": self.id_people,
+            "name": self.name,
+            "description": self.description,
+            "model": self.model,
+            "parcour": self.parcour,
+            "surface": self.surface
+
+        }
